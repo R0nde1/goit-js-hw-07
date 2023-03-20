4c41cfe0c6363aef0781603d473ffc88aa4849f4
Ronald <rogozinronald@gmail.com>
2023-03-21 00:22:21 +0200

    021

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const divGallery = document.querySelector('.gallery');
+
+const instance = basicLightbox.create(`<img src="">`, {
+  onShow: instance => {
+    document.addEventListener('keydown', onEscKeyPress);
+  },
+
+  onClose: instance => {
+    document.removeEventListener('keydown', onEscKeyPress);
+  },
+});
+
+const markup = galleryItems
+  .map(
+    ({ description, original, preview }) =>
+      `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" ></a></div>`
+  )
+  .join('');
+divGallery.innerHTML = markup;
+
+const onDivGalleryClick = evt => {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  instance.element().querySelector('img').src = evt.target.dataset.source;
+
+  instance.show();
+};
+
+function onEscKeyPress(evt) {
+  if (evt.key === 'Escape') {
+    instance.close();
+    return;
+  }
+}
+divGallery.addEventListener('click', onDivGalleryClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,59 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-// function createGalleryMarkup(items) {
-//     return items
-//       .map(
-//         ({ preview, original, description }) =>
-//           `<li class="gallery__item">
-//              <a class="gallery__link" href="${original}">
-//                <img class="gallery__image" src="${preview}" alt="${description}" />
-//              </a>
-//            </li>`
-//       )
-//       .join('');
-//   }
-//   const galleryContainer = document.querySelector('.gallery');
-//   galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
 
-//   const gallery = new SimpleLightbox('.gallery a', {
-//     captionsData: 'alt',
-//     captionDelay: 250,
-//   });
-  
-import { galleryItems } from './gallery-items.js';
-
-const galleryContainer = document.querySelector('.gallery');
-
-function createGalleryItem(item) {
-  const galleryItem = document.createElement('li');
-  galleryItem.classList.add('gallery__item');
-
-  const galleryLink = document.createElement('a');
-  galleryLink.classList.add('gallery__link');
-  galleryLink.setAttribute('href', item.original);
-
-  const galleryImage = document.createElement('img');
-  galleryImage.classList.add('gallery__image');
-  galleryImage.setAttribute('src', item.preview);
-  galleryImage.setAttribute('alt', item.description);
-
-  galleryLink.appendChild(galleryImage);
-  galleryItem.appendChild(galleryLink);
-
-  return galleryItem;
-}
-
-function renderGalleryItems(items) {
-  const galleryItems = items.map(item => createGalleryItem(item));
-  galleryContainer.append(...galleryItems);
-}
-
-renderGalleryItems(galleryItems);
-
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250
-});
-    
 
 console.log(galleryItems);
+const divGallery = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    ({ description, original, preview }) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"></a></li>`
+  )
+  .join('');
+divGallery.innerHTML = markup;
+
+let gallery = new SimpleLightbox('.gallery a', {
+ 
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
